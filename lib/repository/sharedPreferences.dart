04f2419dd883509513768ac87d443e2de2259706
chotixity <melkychoti@gmail.com>
2024-05-27 13:02:10 +0300

    User account screen done

---
+++ lib/repository/sharedPreferences.dart
@@ -0,0 +1,9 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class sharedpreferencesStorage {
+  final _prefs = SharedPreferences.getInstance();
+
+  void setUserDetails() {
+    //_prefs.se
+  }
+}
